f20e740379be6b1045cea737f13f4defb423cb51
r0binSahin <robin.sahin88@gmail.com>
2024-02-06 08:51:11 +0100

    (refactor): cleaning

--- fed22s-therestaurant/src/components/NavbarMenu.tsx
+++ fed22s-therestaurant/src/components/NavbarMenu.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import '../navbar.css';
-import { Link } from 'react-router-dom';
-import { Title } from './styled/Title';
+import { useEffect, useState } from "react";
+import "../navbar.css";
+import { Link } from "react-router-dom";
+import { Title } from "./styled/Title";
 
 export const NavbarMenu = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -13,70 +13,64 @@ export const NavbarMenu = () => {
     setEase(!ease);
   };
 
-  useEffect(() => {
+  /*   useEffect(() => {
     const changeWidth = () => {
       setScreenWidth(window.innerWidth);
     };
 
-    window.addEventListener('resize', changeWidth);
+    window.addEventListener("resize", changeWidth);
 
     return () => {
-      window.removeEventListener('resize', changeWidth);
+      window.removeEventListener("resize", changeWidth);
     };
-  }, []);
+  }, []); */
 
   return (
     <>
       <nav>
-        <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
-          {' '}
+        <Link to="/" style={{ textDecoration: "none", color: "white" }}>
           <Title>SUSHIHAKET </Title>
         </Link>
 
         {
-          <>
-            {' '}
-            <ul className={ease ? 'list list-ease' : 'list list-hidden'}>
-              <li className='items'>
-                {' '}
-                <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
-                  startsida
-                </Link>
-              </li>
-              <li className='items'>
-                {' '}
-                <Link
-                  to='/meny'
-                  style={{ textDecoration: 'none', color: 'white' }}
-                >
-                  meny
-                </Link>
-              </li>
-              <li className='items'>
-                <Link
-                  to='/bokabord'
-                  style={{ textDecoration: 'none', color: 'white' }}
-                >
-                  boka bord
-                </Link>
-              </li>
-              <li className='items'>
-                <Link
-                  to='/admin'
-                  style={{ textDecoration: 'none', color: 'white' }}
-                >
-                  admin
-                </Link>
-              </li>
-            </ul>
-          </>
+          <ul className={ease ? "list list-ease" : "list list-hidden"}>
+            <li className="items">
+              <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                startsida
+              </Link>
+            </li>
+            <li className="items">
+              <Link
+                to="/meny"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                meny
+              </Link>
+            </li>
+            <li className="items">
+              <Link
+                to="/bokabord"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                boka bord
+              </Link>
+            </li>
+            <li className="items">
+              <Link
+                to="/admin"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                admin
+              </Link>
+            </li>
+          </ul>
         }
 
-        <button onClick={toggleNav} className='btn'>
+        <button onClick={toggleNav} className="btn">
           <img
-            className='sushi'
-            src='https://i.postimg.cc/qv8WY6kS/sushimenu.png'
-            alt='sushimenu'
+            className="sushi"
+            src="https://i.postimg.cc/qv8WY6kS/sushimenu.png"
+            alt="sushimenu"
           />
         </button>
       </nav>
